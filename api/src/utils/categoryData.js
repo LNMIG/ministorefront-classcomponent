9a4b245f707f46a5b35f30b1f0f154b7c3595b8a
LNMIG <migliorenicolas@yahoo.com.ar>
2022-09-06 12:13:47 -0300

    DOCS: data for seeder modularized

---
+++ api/src/utils/categoryData.js
@@ -0,0 +1,5 @@
+export const categoryData = [
+    {name: 'all'},
+    {name: 'clothes'},
+    {name: 'tech'}
+]
---
+++ api/src/utils/currencyData.js
@@ -0,0 +1,7 @@
+export const currencyData = [
+    {symbol: '$', label: 'USD'},
+    {symbol: '£', label: 'GBP'},
+    {symbol: 'A$', label: 'AUD'},
+    {symbol: '¥', label: 'JPY'},
+    {symbol: '₽', label: 'RUB'}
+]
