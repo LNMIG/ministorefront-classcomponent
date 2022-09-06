59ba3ccf22e067202d8ca610f9dbc33915750160
LNMIG <migliorenicolas@yahoo.com.ar>
2022-09-05 23:00:38 -0300

    DOCS: resolver cleaned

--- api/src/resolvers/resolvers.js
+++ api/src/resolvers/resolvers.js
@@ -1,14 +1,8 @@
 import { mutations } from '../mutations/mutations.js'
 import { queries } from '../queries/queries.js'
-// import { Course } from './types.js'
-// import { Person } from './types.js'
-// import { GlobalSearch } from './types.js'
 
 export const resolvers =
 {
     Query: queries,
     Mutation: mutations,
-    // Course,
-    // Person,
-    // GlobalSearch
 }
