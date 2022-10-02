a18c33edbd51124ea39c8127317789e2dc4a5d97
LNMIG <migliorenicolas@yahoo.com.ar>
2022-10-02 15:17:36 -0300

    DOCS: main files modified / updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import { Component } from "react"
-import { Route, Switch } from "react-router-dom"
+import React, { Component } from 'react';
+import { Route, Switch } from "react-router-dom";
 
-import Main from './pages/main/main'
-class App extends Component{
+import Cart from './pages/cart/cart'
+import Error404 from './pages/error404/error404';
+import LandingPage from './pages/landing/landing'
+import CheckoutMain from './pages/checkout/checkoutMain';
+import ProductsList from "./pages/productList/productList";
+import ProductDetail from './pages/productDetail/productDetail';
+
+class App extends Component {
   render () {
       return (
         <div>
           <Switch>
-            <Route exact path='/' component={ Main } />
-
-            {/* <Route path='/*' component={ Error404 } /> */}
-
+            <Route exact path='/' component={ LandingPage } />
+            <Route exact path='/productslist/:category' component={ ProductsList } />
+            <Route exact path='/productdetails/:id' component={ ProductDetail } />
+            <Route exact path='/cart' component={ Cart } />
+            <Route exact path='/checkout' component={ CheckoutMain }/>
+            <Route path='/*' component={ Error404 } />
           </Switch>
         </div>
       )
--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import store from './redux/store'
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 )
 
